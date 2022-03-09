651ab1e77c4bfb91fbd9e109b80454b49eefa92e
thao <vuthao0410@gmail.com>
2022-03-09 08:53:13 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register-login/register/register.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './register-login/login/login.component';
+import { NavbarComponent } from './shared/navbar-footer/navbar/navbar.component';
+import { FooterComponent } from './shared/navbar-footer/footer/footer.component';
+import { RegisterCompanyComponent } from './register-login/register-company/register-company.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    HomeComponent,
+    LoginComponent,
+    NavbarComponent,
+    FooterComponent,
+    RegisterCompanyComponent,
+
   ],
   imports: [
     BrowserModule,

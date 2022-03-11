8be33623e6ce40d1786c858745b2db5d3c338630
thao <vuthao0410@gmail.com>
2022-03-11 14:23:32 +0700

    rollback

---
+++ src/app/service/custom-validation.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {FormGroup} from "@angular/forms";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomValidationService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  passwordMatchValidator(password: string, confirmPassword: string) {
+    // @ts-ignore
+    return (formGroup: FormGroup) => {
+      const passwordControl = formGroup.controls[password];
+      const confirmPasswordControl = formGroup.controls[confirmPassword];
+
+      if (!passwordControl || !confirmPasswordControl) {
+        return null;
+      }
+
+      if (
+        confirmPasswordControl.errors &&
+        !confirmPasswordControl.errors.passwordMismatch
+      ) {
+        return null;
+      }
+
+      if (passwordControl.value !== confirmPasswordControl.value) {
+        confirmPasswordControl.setErrors({passwordMismatch: true});
+      } else {
+        confirmPasswordControl.setErrors(null);
+      }
+    };
+  }
+}
